3ef4cefb1076af3ef5906dc2a1863daec5805776
ionchik <ionchik@duck.com>
2025-03-25 14:48:24 +0300

    Art service's delete comment command was done.

---
+++ ArtService/ArtService.Application/Comments/Commands/DeleteComment/DeleteCommentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Comments.Commands.DeleteComment
+{
+    public class DeleteCommentCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Comments/Commands/DeleteComment/DeleteCommentCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Comments.Commands.DeleteComment
+{
+    public class DeleteCommentCommandHandler(IArtServiceDbContext dbContext)
+        : IRequestHandler<DeleteCommentCommand>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+
+        public async Task Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Comments
+                .FirstOrDefaultAsync(comment => comment.Id == request.Id,
+                cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Comment), request.Id);
+            }
+
+            _dbContext.Comments.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
