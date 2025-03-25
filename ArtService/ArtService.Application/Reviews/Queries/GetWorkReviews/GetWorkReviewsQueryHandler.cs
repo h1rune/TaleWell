8c18b6ccdfe4f8ad2ec53db9985eb7b7bfee9d99
ionchik <ionchik@duck.com>
2025-03-25 21:28:22 +0300

    Art service's get work reviews query was done.

---
+++ ArtService/ArtService.Application/Reviews/Queries/GetWorkReviews/GetWorkReviewsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ArtService.Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Reviews.Queries.GetWorkReviews
+{
+    public class GetWorkReviewsQueryHandler(IArtServiceDbContext dbContext, IMapper mapper)
+        : IRequestHandler<GetWorkReviewsQuery, WorkReviewsVm>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<WorkReviewsVm> Handle(GetWorkReviewsQuery request, CancellationToken cancellationToken)
+        {
+            var reviewsQuery = await _dbContext.Reviews
+                .Where(review => review.WorkId == request.WorkId)
+                .ProjectTo<ReviewLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return new WorkReviewsVm { Reviews = reviewsQuery };
+        }
+    }
+}
---
+++ ArtService/ArtService.Application/Reviews/Queries/GetWorkReviews/WorkReviewsVm.cs
@@ -0,0 +1,7 @@
+﻿namespace ArtService.Application.Reviews.Queries.GetWorkReviews
+{
+    public class WorkReviewsVm
+    {
+        public required IList<ReviewLookupDto> Reviews { get; set; }
+    }
+}
