28a3cc2bf699947926189ce9112180508f0e5c40
ionchik <ionchik@duck.com>
2025-03-25 21:12:22 +0300

    Art service's delete review command was done.

---
+++ ArtService/ArtService.Application/Reviews/Commands/DeleteReview/DeleteReviewCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Reviews.Commands.DeleteReview
+{
+    public class DeleteReviewCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
