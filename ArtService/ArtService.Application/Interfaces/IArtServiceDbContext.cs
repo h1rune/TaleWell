0dd2f69973091866b0b6a1aa8083fb899db7f58a
ionchik <ionchik@duck.com>
2025-03-25 11:19:41 +0300

    Art service's database context was added.

---
+++ ArtService/ArtService.Application/Interfaces/IArtServiceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ArtService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Interfaces
+{
+    public interface IArtServiceDbContext
+    {
+        DbSet<Work> Works { get; set; }
+        DbSet<Review> Reviews { get; set; }
+        DbSet<Chapter> Chapters { get; set; }
+        DbSet<Paragraph> Paragraphs { get; set; }
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Reaction> Reactions { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
