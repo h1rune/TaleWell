9518927a79b7670de00f5531fbb3ddead85aba03
ionchik <ionchik@duck.com>
2025-03-25 14:37:38 +0300

    Art service's update comment command was done.

---
+++ ArtService/ArtService.Application/Comments/Commands/UpdateComment/UpdateCommentCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Comments.Commands.UpdateComment
+{
+    public class UpdateCommentCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public required string Text { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Comments/Commands/UpdateComment/UpdateCommentCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Comments.Commands.UpdateComment
+{
+    public class UpdateCommentCommandHandler(IArtServiceDbContext dbContext) : IRequestHandler<UpdateCommentCommand>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+
+        public async Task Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Comments
+                .FirstOrDefaultAsync(comment => comment.Id == request.Id, 
+                cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Comment), request.Id);
+            } 
+
+            entity.Text = request.Text;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
