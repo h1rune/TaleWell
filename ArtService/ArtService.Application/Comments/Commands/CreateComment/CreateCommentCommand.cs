2fde94d2b66ad78f60319d86989f2cdac95ffdae
ionchik <ionchik@duck.com>
2025-03-25 14:21:18 +0300

    Art service's create comment command was done.

---
+++ ArtService/ArtService.Application/Comments/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Comments.Commands.CreateComment
+{
+    public class CreateCommentCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid ParagraphId { get; set; }
+        public required string Text { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Comments/Commands/CreateComment/CreateCommentCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+
+namespace ArtService.Application.Comments.Commands.CreateComment
+{
+    public class CreateCommentCommandHandler(IArtServiceDbContext dbContext) 
+        : IRequestHandler<CreateCommentCommand, Guid>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+        {
+            var comment = new Comment
+            {
+                Id = new Guid(),
+                UserId = request.UserId,
+                ParagraphId = request.ParagraphId,
+                Text = request.Text,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _dbContext.Comments.AddAsync(comment, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return comment.Id;
+        }
+    }
+}
