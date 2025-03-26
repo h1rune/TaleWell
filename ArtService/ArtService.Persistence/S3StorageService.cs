85724f4d7f3fa4614f6f98f491736faf46428bdf
ionchik <ionchik@duck.com>
2025-03-26 12:45:00 +0300

    Art service's external storage infrastructure was done.

---
+++ ArtService/ArtService.Persistence/S3StorageService.cs
@@ -0,0 +1,53 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using ArtService.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace ArtService.Persistence
+{
+    public class S3StorageService : IStorageService
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public S3StorageService(IAmazonS3 s3Client, IConfiguration configuration)
+        {
+            var accessKey = configuration["S3:AccessKey"];
+            var secretKey = configuration["S3:SecretKey"];
+            var region = configuration["S3:Region"];
+            _bucketName = configuration["S3:BucketName"]!;
+
+            _s3Client = new AmazonS3Client(accessKey, secretKey, RegionEndpoint.GetBySystemName(region));
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file, string path)
+        {
+            using var stream = file.OpenReadStream();
+            var request = new PutObjectRequest
+            {
+                BucketName = _bucketName,
+                Key = path,
+                InputStream = stream,
+                ContentType = file.ContentType
+            };
+
+            await _s3Client.PutObjectAsync(request);
+            return path;
+        }
+
+        public string GeneratePreSignedUrl(string key, TimeSpan expiration)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = _bucketName,
+                Key = key,
+                Expires = DateTime.UtcNow.Add(expiration)
+            };
+
+            return _s3Client.GetPreSignedURL(request);
+        }
+    }
+
+}
