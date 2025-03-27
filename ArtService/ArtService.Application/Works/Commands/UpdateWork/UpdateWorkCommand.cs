6a7fad71a8a362630b8bb6171b19e306384eb85b
ionchik <ionchik@duck.com>
2025-03-27 14:33:20 +0300

    Art service's update work command was done.

---
+++ ArtService/ArtService.Application/Works/Commands/UpdateWork/UpdateWorkCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Works.Commands.UpdateWork
+{
+    public class UpdateWorkCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public bool IsFanfic { get; set; }
+        public Guid? OriginalWorkId { get; set; }
+    }
+}
