e38971f4af5170a94a11f8cadb98499c97bf100e
ionchik <ionchik@duck.com>
2025-03-27 14:04:04 +0300

    Art service's update chapter command was done.

---
+++ ArtService/ArtService.Application/Chapters/Commands/UpdateChapter/UpdateChapterCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Chapters.Commands.UpdateChapter
+{
+    public class UpdateChapterCommand : IRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+
+        public int Order { get; set; }
+        public string? Title { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Chapters/Commands/UpdateChapter/UpdateChapterCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Chapters.Commands.UpdateChapter
+{
+    public class UpdateChapterCommandHandler(IArtServiceDbContext dbContext)
+        : IRequestHandler<UpdateChapterCommand>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+
+        public async Task Handle(UpdateChapterCommand request, CancellationToken cancellationToken)
+        {
+            var chapter = await _dbContext.Chapters
+                .FirstOrDefaultAsync(chapter => chapter.Id == request.Id, cancellationToken)
+                ?? throw new NotFoundException(nameof(Chapter), request.Id);
+
+            var volume = await _dbContext.Volumes
+                .Include(volume => volume.RelatedWork)
+                .FirstOrDefaultAsync(volume => volume.Id == chapter.VolumeId, cancellationToken)
+                ?? throw new NotFoundException(nameof(Volume), chapter.VolumeId);
+
+            if (volume.RelatedWork == null || volume.RelatedWork.AuthorId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Work), volume.WorkId);
+            }
+
+            chapter.Title = request.Title;
+            chapter.Order = request.Order;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
