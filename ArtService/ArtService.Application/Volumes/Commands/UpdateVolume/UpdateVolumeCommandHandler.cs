589d10ab0db2b71242e78b1ee572011f2fb1d8cd
ionchik <ionchik@duck.com>
2025-03-27 16:27:09 +0300

    Art service's update volume command was done.

---
+++ ArtService/ArtService.Application/Volumes/Commands/UpdateVolume/UpdateVolumeCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Volumes.Commands.UpdateVolume
+{
+    public class UpdateVolumeCommandHandler(IArtServiceDbContext dbContext, IStorageService storageService)
+        : IRequestHandler<UpdateVolumeCommand>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IStorageService _storageService = storageService;
+
+        public async Task Handle(UpdateVolumeCommand request, CancellationToken cancellationToken)
+        {
+            var volume = await _dbContext.Volumes
+                .Include(volume => volume.RelatedWork)
+                .FirstOrDefaultAsync(volume => volume.Id == request.Id, cancellationToken)
+                ?? throw new NotFoundException(nameof(Volume), request.Id);
+
+            if (volume.RelatedWork.AuthorId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Work), volume.WorkId);
+            } 
+
+            volume.Order = request.Order;
+            volume.Title = request.Title;
+
+            if (request.CoverFile != null)
+            {
+                var path = $"works/{volume.WorkId}/covers/{Guid.NewGuid()}-{request.CoverFile.FileName}";
+                var coverKey = await _storageService.UploadFileAsync(request.CoverFile, path, cancellationToken);
+                volume.CoverKey = coverKey;
+            }
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
