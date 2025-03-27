b3f744f70dbc325c925075f39114c34c0dbcbc5a
ionchik <ionchik@duck.com>
2025-03-27 16:33:07 +0300

    Art service's delete volume command was done.

---
+++ ArtService/ArtService.Application/Volumes/Commands/DeleteVolume/DeleteVolumeCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Volumes.Commands.DeleteVolume
+{
+    public class DeleteVolumeCommandHandler(IArtServiceDbContext dbContext, IStorageService storageService)
+        : IRequestHandler<DeleteVolumeCommand>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IStorageService _storageService = storageService;
+
+        public async Task Handle(DeleteVolumeCommand request, CancellationToken cancellationToken)
+        {
+            var volume = await _dbContext.Volumes
+                .Include(volume => volume.RelatedWork)
+                .FirstOrDefaultAsync(volume => volume.Id == request.Id, cancellationToken)
+                ?? throw new NotFoundException(nameof(Volume), request.Id);
+
+            if (volume.RelatedWork.AuthorId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Work), volume.WorkId);
+            }
+
+            if (volume.CoverKey != null)
+            {
+                await _storageService.DeleteFileAsync(volume.CoverKey, cancellationToken);
+            }
+      
+            _dbContext.Volumes.Remove(volume);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
