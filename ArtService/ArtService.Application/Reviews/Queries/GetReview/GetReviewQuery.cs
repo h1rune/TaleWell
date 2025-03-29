2ea2d6a663b5de630c3ef81ea5bbf4b45912d021
ionchik <ionchik@duck.com>
2025-03-29 13:26:12 +0300

    Art service's get review query was done.

---
+++ ArtService/ArtService.Application/Reviews/Queries/GetReview/GetReviewQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ArtService.Application.Reviews.Queries.GetReview
+{
+    public class GetReviewQuery : IRequest<ReviewVm>
+    {
+        public Guid ReviewId { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Reviews/Queries/GetReview/GetReviewQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Reviews.Queries.GetReview
+{
+    public class GetReviewQueryHandler(IArtServiceDbContext dbContext, IMapper mapper)
+        : IRequestHandler<GetReviewQuery, ReviewVm>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<ReviewVm> Handle(GetReviewQuery request, CancellationToken cancellationToken)
+        {
+            var reviewEntity = await _dbContext.Reviews
+                .FirstOrDefaultAsync(review => review.Id == request.ReviewId, cancellationToken)
+                ?? throw new NotFoundException(nameof(Review), request.ReviewId);
+
+            return _mapper.Map<ReviewVm>(reviewEntity);
+        }
+    }
+}
