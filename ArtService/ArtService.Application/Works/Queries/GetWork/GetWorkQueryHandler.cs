78e38593f085bdbc5b11c570f44b1843120aba7e
ionchik <ionchik@duck.com>
2025-03-29 13:55:57 +0300

    Art service's get work query was done.

---
+++ ArtService/ArtService.Application/Works/Queries/GetWork/GetWorkQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Works.Queries.GetWork
+{
+    public class GetWorkQueryHandler(IArtServiceDbContext dbContext, IMapper mapper)
+        : IRequestHandler<GetWorkQuery, WorkVm>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<WorkVm> Handle(GetWorkQuery request, CancellationToken cancellationToken)
+        {
+            var workEntity = await _dbContext.Works
+                .FirstOrDefaultAsync(work => work.Id == request.WorkId, cancellationToken)
+                ?? throw new NotFoundException(nameof(Work), request.WorkId);
+
+            return _mapper.Map<WorkVm>(workEntity);
+        }
+    }
+}
---
+++ ArtService/ArtService.Application/Works/Queries/GetWork/WorkVm.cs
@@ -0,0 +1,27 @@
+﻿using ArtService.Application.Common.Mappings;
+using ArtService.Domain;
+using AutoMapper;
+
+namespace ArtService.Application.Works.Queries.GetWork
+{
+    public class WorkVm : IMapWith<Work>
+    {
+        public Guid AuthorId { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsFanfic { get; set; }
+        public Guid? OriginalWorkId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Work, WorkVm>()
+                .ForMember(fanficDto => fanficDto.AuthorId, options => options.MapFrom(work => work.AuthorId))
+                .ForMember(fanficDto => fanficDto.Title, options => options.MapFrom(work => work.Title))
+                .ForMember(fanficDto => fanficDto.Description, options => options.MapFrom(work => work.Description))
+                .ForMember(fanficDto => fanficDto.IsFanfic, options => options.MapFrom(work => work.IsFanfic))
+                .ForMember(fanficDto => fanficDto.OriginalWorkId, options => options.MapFrom(work => work.OriginalWorkId))
+                .ForMember(fanficDto => fanficDto.CreatedAt, options => options.MapFrom(work => work.CreatedAt));
+        }
+    }
+}
