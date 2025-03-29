ba63928a8c2e307274af482576bff55d0826bbef
ionchik <ionchik@duck.com>
2025-03-29 14:06:23 +0300

    Art service's delete volume command was refreshed.

--- ArtService/ArtService.Application/Volumes/Commands/DeleteVolume/DeleteVolumeCommand.cs
+++ ArtService/ArtService.Application/Volumes/Commands/DeleteVolume/DeleteVolumeCommand.cs
@@ -5,6 +5,6 @@ namespace ArtService.Application.Volumes.Commands.DeleteVolume
     public class DeleteVolumeCommand : IRequest
     {
         public Guid UserId { get; set; }
-        public Guid Id { get; set; }
+        public Guid VolumeId { get; set; }
     }
 }
