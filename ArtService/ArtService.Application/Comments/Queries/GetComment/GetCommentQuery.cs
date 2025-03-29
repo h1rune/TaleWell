911b423d4b196933d11e3458b3ced31dc109fe1d
ionchik <ionchik@duck.com>
2025-03-29 12:34:47 +0300

    Art service's get comment query was done.

---
+++ ArtService/ArtService.Application/Comments/Queries/GetComment/GetCommentQuery.cs
@@ -0,0 +1,10 @@
+﻿using ArtService.Application.Comments.Queries.GetParagraph;
+using MediatR;
+
+namespace ArtService.Application.Comments.Queries.GetComment
+{
+    public class GetCommentQuery : IRequest<CommentVm>
+    {
+        public Guid CommentId { get; set; }
+    }
+}
---
+++ ArtService/ArtService.Application/Comments/Queries/GetComment/GetCommentQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using ArtService.Application.Comments.Queries.GetParagraph;
+using ArtService.Application.Common.Exceptions;
+using ArtService.Application.Interfaces;
+using ArtService.Domain;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtService.Application.Comments.Queries.GetComment
+{
+    public class GetCommentQueryHandler(IArtServiceDbContext dbContext, IMapper mapper)
+        : IRequestHandler<GetCommentQuery, CommentVm>
+    {
+        private readonly IArtServiceDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<CommentVm> Handle(GetCommentQuery request, CancellationToken cancellationToken)
+        {
+            var commentEntity = await _dbContext.Comments
+                .FirstOrDefaultAsync(comment => comment.Id == request.CommentId, cancellationToken)
+                ?? throw new NotFoundException(nameof(Comment), request.CommentId);
+
+            return _mapper.Map<CommentVm>(commentEntity);   
+        }
+    }
+}
