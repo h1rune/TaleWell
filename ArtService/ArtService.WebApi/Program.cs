6051c913514bb0c7ecc33d26320957af1cb0f715
ionchik <ionchik@duck.com>
2025-03-31 15:27:59 +0300

    Art service's env was added.

--- ArtService/ArtService.WebApi/Program.cs
+++ ArtService/ArtService.WebApi/Program.cs
@@ -1,4 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
