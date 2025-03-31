e7e7d20d0b03751306faaad0f9a771a867ed7076
ionchik <ionchik@duck.com>
2025-03-31 15:36:32 +0300

    Art service git was refreshed.

--- ArtService/ArtService.WebApi/Program.cs
+++ ArtService/ArtService.WebApi/Program.cs
@@ -1,12 +1,28 @@
+using ArtService.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
-    .AddEnvironmentVariables();
+.AddEnvironmentVariables();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<ArtServiceDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
