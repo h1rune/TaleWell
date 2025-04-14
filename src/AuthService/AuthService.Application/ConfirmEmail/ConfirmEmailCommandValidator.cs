6f12ad045578c553a2984aa4ea35267feafa6589
ionchik <ionchik@duck.com>
2025-04-14 20:57:13 +0300

    Auth service's confirm email command validation was done.

---
+++ src/AuthService/AuthService.Application/ConfirmEmail/ConfirmEmailCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace AuthService.Application.ConfirmEmail
+{
+    public class ConfirmEmailCommandValidator : AbstractValidator<ConfirmEmailCommand>
+    {
+        public ConfirmEmailCommandValidator()
+        {
+            RuleFor(command => command.AccountId)
+                .NotEmpty().WithMessage("Account ID is required.");
+
+            RuleFor(command => command.Token)
+                .NotEmpty().WithMessage("Token is required.");
+        }
+    }
+}
